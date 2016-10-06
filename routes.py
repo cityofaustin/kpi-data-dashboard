113a31406c2630796326d664c92b2834dbdbdbd1
Hailey Pate <2haileypate@gmail.com>
2016-10-06 16:21:17 -0500

    add json and table to template

--- routes.py
+++ routes.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+import json
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+	with open('data/current_progress.json', 'r') as f:
+		return render_template('index.html', depts=json.loads(f.read()))
 
 
 if __name__ == "__main__":
