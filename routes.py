685065db465c256061f75f1767b64a2dc7c848f9
Hailey Pate <2haileypate@gmail.com>
2016-10-06 17:00:32 -0500

    add about and find pages

--- routes.py
+++ routes.py
@@ -9,6 +9,13 @@ def index():
 	with open('data/current_progress.json', 'r') as f:
 		return render_template('index.html', depts=json.loads(f.read()))
 
+@app.route("/about")
+def about():
+	return render_template('about.html')
+
+@app.route("/find")
+def find_page():
+	return render_template('find.html')
 
 if __name__ == "__main__":
 	app.run(debug=True)
