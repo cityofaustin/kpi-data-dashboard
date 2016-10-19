0e4d951101ef216259a7cb9084b50fb8ab64b408
Hailey Pate <2haileypate@gmail.com>
2016-10-19 12:07:32 -0500

    info arch overhaul
    lots of changes, sloppy committing. sorry y’all...

--- routes.py
+++ routes.py
@@ -4,19 +4,24 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-	with open('data/current_progress.json', 'r') as f:
-		return render_template('index.html', depts=json.loads(f.read()))
+	return render_template('index.html')
 
-@app.route("/about")
-def about():
-	return render_template('about.html')
+@app.route("/progress")
+def progress():
+	with open('data/dept_stats.json', 'r') as f:
+		return render_template('progress.html', depts=json.loads(f.read()))
 
-@app.route("/find")
-def find_page():
+@app.route("/help")
+def help():
+	return render_template('help.html')
+
+@app.route("/participate")
+def participate():
 	with open('data/survey_stats.json', 'r') as f:
-		return render_template('find.html', measures=json.loads(f.read()))
+		return render_template('participate.html', measures=json.loads(f.read()))
 
 if __name__ == "__main__":
 	app.run(debug=True)
